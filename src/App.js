2a3b801769324c9b9d649ec85958e7a1b2d5f21d
Mru-exe <58219280+Mru-exe@users.noreply.github.com>
2025-04-24 20:44:48 +0200

    Refactored and added type safety

--- src/App.js
+++ src/App.js
@@ -1,94 +1,78 @@
+//@ts-check
+
 import EventBus from './EventBus.js';
 import Editor from './Editor.js';
 import Parser from './Parser.js';
 import Renderer from './Renderer.js';
 
-/* export class Application {
-  #editor;
-  #parser;
-  #canvas;
-
-  database;
-  #inputTimeout;
-
-  constructor(editorElement, canvasElement) {
-    this.#canvas = new Canvas(canvasElement);
-    this.#editor = new Editor(editorElement);
-    this.#parser = new Parser();
-    this.database = null;
-
-    this.#editor.element.addEventListener('keyup', () => {
-      if (this.#inputTimeout) {
-        clearTimeout(this.#inputTimeout);
-      }
-      this.#inputTimeout = setTimeout(() => {
-        this.handleInput();
-      }, 500);
-      // this.handleInput();
-    });
-  }
-
-  handleInput(){
-    let parsedDb;
-    try {
-      parsedDb = this.#parser.parseDBML(this.#editor.getValue());
-    } catch (error) {
-      // editor.handleSyntaxError(error);
-      console.error("hadnleInput() thrown: ", JSON.stringify(error));
-      return;
-    }
-    if(parsedDb.schemas.length == 0){
-      return;
-    }
-    console.log("Parsed DBML of type" + typeof parsedDb + ": "+ JSON.stringify(parsedDb));
-    this.#canvas.plot(parsedDb);
-  }
-
-  run() {
-    // console.info('App is running');
-  }
-
-  notificationTest(){
-    console.log("user inpu");
-  }
-
-  handleErrors(){
-
-  }
-
-  // parseDBML() {
-  //   const dbml = this.editor.getValue();
-  //   try {
-  //     const parsed = this.parser.parse(dbml, 'dbml');
-  //     this.database = parsed.schemas[0];
-  //     console.log(this.database.tables);
-  //   } catch (e) {
-  //     console.error(e);
-  //     throw e;
-  //   }
-  // }
-} 
-*/
-
 export class Application {
-  _editor;
-  _renderer;
-  _parser;
-  _eventBus;
+  /**
+   * @type {Editor}
+   * @private
+   */
+  editor;
+
+  /**
+   * @type {Renderer}
+   * @private
+   */
+  renderer;
+
+  /**
+   * @type {Parser}
+   * @private
+   */
+  parser;
 
   constructor(){
-    this.eventBus = EventBus;
-    this._parser = new Parser();
+    this.parser = new Parser();
   }
 
-  run(editorElement, canvasElement){
-    this._editor = new Editor(editorElement);
-    this._renderer = new Renderer(canvasElement);
+  /**
+   * TODO!
+   * @description Runs the application.
+   */
+  run(inputElement, canvasElement){
+    //TODO: param types & doc
+    this.editor = new Editor(EventBus, inputElement);
+    this.renderer = new Renderer(canvasElement);
 
-    this.#setupListeners();
+    this.setupListeners();
   }
 
-  #setupListeners(){
-    //TODO
+  getEventBus(){
+    return EventBus;
+  }
+
+  /**
+   * 
+   * @param {String} input User input
+   * @returns {void}
+   * @private
+   */
+  handleInputChange(input){
+    if(input == null || input.length == 0){ 
+      this.renderer.clear(); 
+      return; 
+    }
+    
+    try {
+      const db = this.parser.parseDatabase(input);
+      if(db == null){
+        console.warn("db is null");
+        return;
+      }
+      this.renderer.plot(db);
+    } catch (error) {
+      EventBus.notify('parseError', error);
+    }
+  }
+
+  /**
+   * @returns {void}
+   * @private
+   */
+  setupListeners(){
+    EventBus.subscribe('inputChanged', (e) => { this.handleInputChange(e) } );
   }
 }
