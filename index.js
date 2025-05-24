bcac42af2fb9975cc32d500799a0487948bf2853
Mru-exe <58219280+Mru-exe@users.noreply.github.com>
2025-05-24 14:23:57 +0200

    Renamed main.js to index.js, added router initialization.

---
+++ index.js
@@ -0,0 +1,9 @@
+import { Router } from './Router.js';
+
+const routes = {
+    '/': () => import('./views/LandingView.js'),
+    '/diagram':() => import('./views/AppView.js')
+};
+
+const router = new Router('root-container', routes);
+router.start();
